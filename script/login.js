83c29596572d3d3b4903975d85c3a400e0868e5b
Gaurav kumar <ergauravkumar02071998@gmail.com>
2023-02-18 15:14:15 +0530

    commitToCode

--- script/login.js
+++ script/login.js
@@ -1,41 +1,46 @@
 console.log("login js");
 import Form from "../Component/form.js";
 import Navbar from "../Component/Navbar.js";
-
+// adding navbar import from Navbar.js
 const navBox = document.querySelector("#nav");
 navBox.innerHTML = Navbar();
-let formBox=document.querySelector("#form-box");
-formBox.innerHTML=Form();
+// adding form import from Form.js
+let formBox = document.querySelector("#form-box");
+formBox.innerHTML = Form();
 
+let form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let username = form.username.value;
+  let password = form.password.value;
+//   if input is empty
+  if (!username || !password) {
+    alert("Please enter a username and password");
+    return;
+  }
+//   after submit button
+  handelSubmit(username, password);
+});
 
-let form=document.querySelector("#form");
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let username=form.username.value;
-    let password=form.password.value;
-    if(!username || !password){
-        alert("Please enter a username and password")
-        return 
-    }
-    handelSubmit(username,password)
-})
- 
-
-const handelSubmit=(username,password)=>{
- let validate=check(username,password)
- if(validate){
-   alert("Validation sucess")
- }else{
-    alert("False validation sucess")
- }
-}
-
-const check=(username,password)=>{
-    let usersDetails = JSON.parse(localStorage.getItem("usersDetails")) || [];
-    console.log(usersDetails)
-let data= usersDetails.filter((e)=>e.username==username&&e.password==password)
- if(data.length>0){
-    return true
- }
- return false;
-}
+const handelSubmit = (username, password) => {
+    // check password and username is correct or not
+  let validate = check(username, password);
+  if (validate) {
+    alert("Validation sucess");
+  } else {
+    alert("False validation sucess");
+  }
+};
+// check password and username function
+const check = (username, password) => {
+    // get details list users
+  let usersDetails = JSON.parse(localStorage.getItem("usersDetails")) || [];
+//   console.log(usersDetails);
+  let data = usersDetails.filter(
+    (e) => e.username == username && e.password == password
+  );
+  if (data.length > 0) {
+    return true;
+  }
+  return false;
+};
--- script/signup.js
+++ script/signup.js
@@ -1,32 +1,36 @@
 console.log("signup.js");
 
 import Navbar from "../Component/Navbar.js";
-
+// add navbar import from Navbar.js
 const navBox = document.querySelector("#nav");
 navBox.innerHTML = Navbar();
-
+// select form tag  
 let form = document.querySelector("#form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // onSubmit this function is called
   handelSubmit();
 });
 const handelSubmit = () => {
   let username = form.username.value;
   let password = form.password.value;
+  // if username or password inputs fields are empty
   if (!username || !password) {
     alert("Please enter valid username and password");
     return;
   } else {
+    // Check if username or password is already registered or not
     let registered = checkuser(username, password);
     if (registered) {
       alert("Already Registered");
     } else {
+      // add new user data
       addUser(username, password);
     }
   }
   console.log(username, password);
 };
-
+// check user is already registered or not
 const checkuser = (username) => {
   let usersDetails = JSON.parse(localStorage.getItem("usersDetails")) || [];
   let data = usersDetails.filter((e) => e.username == username);
@@ -35,7 +39,7 @@ const checkuser = (username) => {
   }
   return false;
 };
-
+// add new user to local storage with key =  usersDetails
 let addUser = (username, password) => {
   let usersDetails = JSON.parse(localStorage.getItem("usersDetails")) || [];
   usersDetails.push({ username: username, password: password });
